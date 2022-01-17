c5a629c47caba9be7849d4d6bdcb3430046add4c
Seth <sethpcasey@gmail.com>
2022-01-17 00:36:47 -0500

    Figured out grid system with variable input

--- programmingpages/etchasketch/etchasketch.js
+++ programmingpages/etchasketch/etchasketch.js
@@ -3,34 +3,42 @@
 const keepGoing = 1; //for loop control
 
 const grid = document.querySelector('#game');
+
 grid.style.display = 'flex';
-grid.style.gap = '5px';
-const line = document.createElement('div');
+grid.style.flexDirection = 'column';
+const line = [];
+//const line = document.createElement('div');
+//need to move this createElement somewhere more productive!
 
 createSquare = (lineNumber) => { //creates a single div as defined within
     const square = document.createElement('div');
     square.classList.add('square');
-    square.classList.add(lineNumber);
-    square.textContent = "Hellow world!";
-    line.appendChild(square);
+    square.setAttribute('style', 'border: 25px solid white;');
+    square.style.margin = '1px';
+    line[lineNumber].appendChild(square);
 };
 
 createRowOfSquares = (input) => {
-    for (i = 0; i <= input; i++) {
+    for (i = 0; i < input; i++) {
         createSquare(input);
     }
 };
 
 divCreator = (lineNumber) => {
-    line.classList.add(lineNumber);
-    game.appendChild(line);
+    line[lineNumber] = document.createElement('div');
+    line[lineNumber].classList.add(lineNumber);
+    line[lineNumber].style.display = 'flex';
+    line[lineNumber].style.flexDirection = 'row';
+    line[lineNumber].style.flexWrap = 'nowrap';
+    game.appendChild(line[lineNumber]);
 }
 
 createRows = (numberOfRows) => {
+    line[numberOfRows];
     for (k = 0; k < numberOfRows; k++) {
         divCreator(numberOfRows);
         createRowOfSquares(numberOfRows);
     }
 };
 
-createRows(2);
+createRows(4);
