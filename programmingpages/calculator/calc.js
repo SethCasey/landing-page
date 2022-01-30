b78e08d3abd8af89eb2f239a5b3376036f88677e
Seth <sethpcasey@gmail.com>
2022-01-30 02:47:39 -0500

    Working on functions for calculator

---
+++ programmingpages/calculator/calc.js
@@ -0,0 +1,23 @@
+const add = (...input) => {
+    let sum = 0;
+    for (let i of input) {
+        sum += i;
+    }
+    return sum;
+}
+
+const subtract = (...input) => {
+    
+}
+
+const multiply = (...input) => {
+    let result = 1;
+    for (let i of input) {
+        result = result * i;
+    }
+    return result;
+}
+
+const divide = (...input) => {
+
+}
