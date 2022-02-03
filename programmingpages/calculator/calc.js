a71a06c307a668d026bae511bee634e4e3c5c4b5
Seth <sethpcasey@gmail.com>
2022-02-02 19:19:26 -0500

    Fleshed out HTML, now need to work on JS for calculator

--- programmingpages/calculator/calc.js
+++ programmingpages/calculator/calc.js
@@ -1,23 +1,41 @@
-const add = (...input) => {
-    let sum = 0;
-    for (let i of input) {
-        sum += i;
-    }
-    return sum;
-}
+const add = (a, b) => {
+    return (a + b);
+};
 
-const subtract = (...input) => {
-    
-}
+const subtract = (a, b) => {
+    return (a - b);
+};
 
-const multiply = (...input) => {
-    let result = 1;
-    for (let i of input) {
-        result = result * i;
-    }
+const multiply = (a, b) => {
+    return (a * b);
+};
+
+const divide = (a, b) => {
+    return (a / b);
+};
+
+const operate = (doStuff, a, b) => {
+    let result = doStuff(a, b);
     return result;
-}
+};
 
-const divide = (...input) => {
+let lastNumber = null;
+let nextNumber = null;
+let selectedNumber = null;
+let //need to do document.getElementsByClassName('numberButton')
+//need to do document.getElementBy????????????
+//need some better variables I think
 
-}
+numberButton.addEventListener('click', () => {
+    if (lastNumber == null) {
+        lastNumber = numberButton.value;
+    } else {
+        nextNumber = numberButton.value;
+    }
+    console.log(lastNumber);
+    console.log(nextNumber);
+});
+
+addition.addEventListener('click', add()) = () => {
+
+};
