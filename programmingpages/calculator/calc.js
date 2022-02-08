409ebebeb8ebe89ce22de22ffe1f1e718b03fb65
Seth <sethpcasey@gmail.com>
2022-02-07 21:30:52 -0500

    Display, clearButton, addition is working

--- programmingpages/calculator/calc.js
+++ programmingpages/calculator/calc.js
@@ -24,9 +24,10 @@ let nextNumber = null;
 let selectedNumber = null;
 let currentSelection;
 let numberButtons = (document.querySelectorAll(".numberButton"))
-let result = document.querySelector(".result");
 let addButton = document.querySelector("#addButton");
 let numberField = document.querySelector(".numberField");
+let display = document.querySelector(".result")
+let clear = document.querySelector("#clearButton")
 
 numberButtons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -42,10 +43,33 @@ numberField.addEventListener('click', () => {
     }
     console.log(lastNumber);
     console.log(nextNumber);
+    updateDisplay(currentSelection);
 });
 
+//errm okay so this works, but... I need it to update
+//after I've hit equals, not before... hmm.
+//Okay, I need to move the actual math to the equals 
+//button? somehow... And pass the operation in too?
 addButton.addEventListener('click', () => {
     if (nextNumber == null) {
         result = operate(add(lastNumber, lastNumber));
+    } else if (nextNumber && lastNumber) {
+        
     }
 });
+
+clear.addEventListener('click', () => {
+    lastNumber = null;
+    nextNumber = null;
+    console.log(lastNumber);
+    console.log(nextNumber);
+    updateDisplay("Cleared!");
+});
+
+const updateDisplay = (input, operation) => {
+    if (input && operation) {
+        display.textContent += `${operation} ${input}`;
+    } else {
+        display.textContent = input;
+    }
+};
