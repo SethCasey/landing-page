0d80f8fac0f902b6277eff83058eadfa3ff1ef2e
Seth <seth01000011@gmail.com>
2022-11-30 19:54:04 -0500

    Added ship list to Player class

--- programmingpages/battleship/player.test.js
+++ programmingpages/battleship/player.test.js
@@ -1,3 +1,12 @@
 import { Player } from "./src/player.js"
 
-test("")
+test("Player's predefined ships are accessible", () => {
+  let newplayer = new Player;
+  
+  expect(newplayer.shipDict.carrier).toEqual(5);
+  expect(newplayer.shipDict.battleship).toEqual(4);
+  expect(newplayer.shipDict.destroyer).toEqual(3);
+  expect(newplayer.shipDict.submarine).toEqual(3);
+  expect(newplayer.shipDict.patrolboat).toEqual(2);
+}
+)
--- programmingpages/battleship/src/player.js
+++ programmingpages/battleship/src/player.js
@@ -1,11 +1,28 @@
+import {Gameboard} from "./gameboard.js"
+import {Ship} from "./shipMaker.js"
 // Create Player.
 
 // should each player be its own object, or should all players be under 
 // "players" object to enable automagic incrementing? default as computer?
+
 export class Player { 
   constructor(playername = "Player") {
     this.playername = playername;
-  }
+    this.ships = [];
+  // tie a set of ships to a player
+    // this.shipDict;
+
+
+    }
+
+  shipDict = {
+    "carrier": 5,
+    "battleship": 4,
+    "destroyer": 3,
+    "submarine": 3,
+    "patrolboat": 2,
+  }  
+  
 
 }
 
